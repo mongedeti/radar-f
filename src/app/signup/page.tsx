eecd4da8dae72e40d1dc5a279db2a854cdb8cc5a
mongedeti <160082855+mongedeti@users.noreply.github.com>
2026-06-03 14:27:12 -0300

    Update page.tsx

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -28,7 +28,7 @@ export default function SignupPage() {
 
       <div className="auth-card">
 
-        <div className="auth-title">Radar C</div>
+        <div className="auth-title">Radar F</div>
         <div className="auth-subtitle">
           Crie sua conta gratuita
         </div>
