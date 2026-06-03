0e7e0be4030762c3408b3061c6005bbcc381b7bd
mongedeti <160082855+mongedeti@users.noreply.github.com>
2026-06-03 14:32:42 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import Link from 'next/link'
 export default function Home() {
 return ( <div className="landing-container">
 
-```
   {/* HEADER */}
   <header className="landing-header">
     <div className="logo">Radar F</div>
@@ -142,7 +141,5 @@ return ( <div className="landing-container">
   </section>
 
 </div>
-```
-
 );
 }
