6d6ce5881a0ecbff39135939c6fae6860966491e
mongedeti <160082855+mongedeti@users.noreply.github.com>
2026-06-03 15:33:43 -0300

    Update page.tsx

--- src/app/vacations/page.tsx
+++ src/app/vacations/page.tsx
@@ -1,1 +1,11 @@
+export default function VacationsPage() {
+  return (
+    <div className="container">
+      <h1>Cadastro de Férias</h1>
 
+      <p>
+        Em construção...
+      </p>
+    </div>
+  )
+}
