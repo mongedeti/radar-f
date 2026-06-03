eb1e0f7b593aea825fec514c9bdc96e25eb097bd
mongedeti <160082855+mongedeti@users.noreply.github.com>
2026-06-03 14:26:46 -0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -28,7 +28,7 @@ export default function LoginPage() {
 
       <div className="auth-card">
 
-        <div className="auth-title">Radar C</div>
+        <div className="auth-title">Radar F</div>
         <div className="auth-subtitle">
           Acesse sua conta
         </div>
